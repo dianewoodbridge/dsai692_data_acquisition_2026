c3c0df430c6e10fa6440ec0b61566e18e3c05f57
Diane Woodbridge <dwoodbridge@usfca.edu>
2026-09-09 10:34:02 -0700

    added StudySession

---
+++ StudySession/2026_09_09/git_exercise.sh
@@ -0,0 +1,10 @@
+# what is git branch
+git branch # branch you're on have *
+# creating branch
+# create a branch called feature/search
+git switch -c feature/search # create a branch and switch to the branch
+git branch feature/search # create a branch
+
+# merging branch
+# merge conflicts and resolve conflicts
+# pull request (PR)
