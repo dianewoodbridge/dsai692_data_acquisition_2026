6ddd8b4d99e75b5f06f00a84102253a5ee909c6c
Diane Woodbridge <dwoodbridge@usfca.edu>
2026-09-09 10:56:06 -0700

    extended git_exercise.sh

--- StudySession/2026_09_09/git_exercise.sh
+++ StudySession/2026_09_09/git_exercise.sh
@@ -7,5 +7,9 @@ git switch -c feature/search # create a branch and switch to the branch
 git branch feature/search # create a branch
 
 # merging branch
+git merge feature/study_session # merge changes on feature/study_session to feature/search
 # merge conflicts and resolve conflicts
+# Open the file with conflicts - inspect lines between >>>>> ,===== and <<<<< 
+# Decide what to keep/how to keep
+
 # pull request (PR)
